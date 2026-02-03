4904eac85a42dcd01ac924533fd0e18e3c2d0e06
gopalsvaid <vaidgopal@gmail.com>
2026-02-03 14:17:05 +0530

    Initial Commit

---
+++ DemoTestProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DemoTestProject
+//
+//  Created by Gopal Vaid on 03/02/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ DemoTestProjectTests/DemoTestProjectTests.swift
@@ -0,0 +1,17 @@
+//
+//  DemoTestProjectTests.swift
+//  DemoTestProjectTests
+//
+//  Created by Gopal Vaid on 03/02/26.
+//
+
+import Testing
+@testable import DemoTestProject
+
+struct DemoTestProjectTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
